cda9ff9bc4e96197a8ed7d62668bf980177ccd6a
JuDirenzo <ext_judirenz@mercadolibre.com>
2021-07-03 06:07:40 -0300

    nuevo test

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let alerta;
+	alerta = prompt("Pone tu nombre");
+	alert(alerta);
 }
 
