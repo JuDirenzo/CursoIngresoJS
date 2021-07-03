ba1aeec8553d18c9c4cd95a47b859ccb4d46beaf
JuDirenzo <ext_judirenz@mercadolibre.com>
2021-07-03 07:30:07 -0300

    solución

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let n1 = txtIdNumeroUno.value; //acá se puede utilizar getElementById("id").value
+	let n2 = txtIdNumeroDos.value;
+
+	n1 = parseInt(n1);
+	n2 = parseInt(n2);
+
+	let result = n1 + n2;
+
+	alert(result);
 }
 
