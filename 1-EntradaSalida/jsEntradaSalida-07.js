f6693288607b85ee07b08a503f310325b64f8756
JuDirenzo <ext_judirenz@mercadolibre.com>
2021-07-04 01:29:40 -0300

    solución

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,11 +4,12 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 
-var n1 = txtIdNumeroUno.value;
-var n2 = txtIdNumeroDos.value; //por qué declarando la variable globalmente esto no funciona??//
+ //por qué declarando la variable globalmente esto no funciona??//
 
 function sumar()
 {	
+	var n1 = txtIdNumeroUno.value;
+	var n2 = txtIdNumeroDos.value;
 	n1 = parseInt(n1);
 	n2 = parseInt(n2);
 
@@ -18,17 +19,42 @@ function sumar()
 }
 
 function restar()
-{
+{ 
+	var n1 = txtIdNumeroUno.value;
+	var n2 = txtIdNumeroDos.value;
+	n1 = parseInt(n1);
+	n2 = parseInt(n2);
+
+	var result = n1 - n2;
+
+	alert("La resta da " + result);
+
 	
 }
 
 function multiplicar()
 { 
+	var n1 = txtIdNumeroUno.value;
+	var n2 = txtIdNumeroDos.value;
+	n1 = parseInt(n1);
+	n2 = parseInt(n2);
+
+	var result = n1 * n2;
+
+	alert("La multiplicación da " + result);
 	
 }
 
 function dividir()
-{
+{	
+	var n1 = txtIdNumeroUno.value;
+	var n2 = txtIdNumeroDos.value;
+	n1 = parseInt(n1);
+	n2 = parseInt(n2);
+
+	var result = n1 / n2;
+
+	alert("La división da " + result);
 	
 }
 
