b99c086cdcc4d8b73734cf7fbc449c802bce2122
JuDirenzo <ext_judirenz@mercadolibre.com>
2021-07-05 05:16:15 -0300

    solución (comentario)

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,57 +4,45 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 
- //por qué declarando la variable globalmente esto no funciona??//
+//por qué declarando la variable globalmente esto no funciona??
+
+// var n1 = parseInt(document.getElementById("txtIdNumeroUno").value); //se puede pasar a Int declarando la variable
+// var n2 = parseInt(document.getElementById("txtIdNumeroDos").value);
 
 function sumar()
 {	
-	var n1 = txtIdNumeroUno.value;
-	var n2 = txtIdNumeroDos.value;
-	n1 = parseInt(n1);
-	n2 = parseInt(n2);
-
-	var result = n1 + n2;
-
-	alert("La suma total es " + result);
+	var n1 = parseInt(txtIdNumeroUno.value);
+	var n2 = parseInt(txtIdNumeroDos.value);
+	var res = n1 + n2;
+	
+	alert("La suma total es " + res);
 }
 
 function restar()
 { 
-	var n1 = txtIdNumeroUno.value;
-	var n2 = txtIdNumeroDos.value;
-	n1 = parseInt(n1);
-	n2 = parseInt(n2);
+	var n1 = parseInt(txtIdNumeroUno.value);
+	var n2 = parseInt(txtIdNumeroDos.value);
+	var res = n1 - n2;
 
-	var result = n1 - n2;
-
-	alert("La resta da " + result);
-
-	
+	alert("La resta da " + res);
 }
 
 function multiplicar()
 { 
-	var n1 = txtIdNumeroUno.value;
-	var n2 = txtIdNumeroDos.value;
-	n1 = parseInt(n1);
-	n2 = parseInt(n2);
-
+	var n1 = parseInt(txtIdNumeroUno.value);
+	var n2 = parseInt(txtIdNumeroDos.value);
 	var result = n1 * n2;
 
-	alert("La multiplicación da " + result);
-	
+	alert("La multiplicación da " + result);	
 }
 
 function dividir()
 {	
-	var n1 = txtIdNumeroUno.value;
-	var n2 = txtIdNumeroDos.value;
-	n1 = parseInt(n1);
-	n2 = parseInt(n2);
-
+	var n1 = parseInt(txtIdNumeroUno.value);
+	var n2 = parseInt(txtIdNumeroDos.value);
 	var result = n1 / n2;
 
 	alert("La división da " + result);
-	
+
 }
 
