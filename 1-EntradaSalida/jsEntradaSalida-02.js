32d053f81c9306c5f779c03b0238ff43871524a6
JuDirenzo <ext_judirenz@mercadolibre.com>
2021-07-07 19:04:22 -0300

    solución

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,10 +1,9 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	let alerta;
-	alerta = prompt("Pone tu nombre");
-	alert(alerta);
+function mostrar() {
+  let nombre;
+  nombre = prompt("Pone tu nombre");
+  alert(nombre);
 }
-
+ 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,11 +3,8 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 
-var nombre;
-nombre = prompt("Ingrese su nombre");
+let nombre = prompt("Ingrese su nombre");
 
-function mostrar()
-{
-	txtIdNombre.value = nombre;
+function mostrar() {
+  txtIdNombre.value = nombre;
 }
-
